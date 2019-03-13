6384e7bf38984fba2b30d71ac939a19816781d8a
Mango <cojjj@163.com>
2019-03-13 21:17:58 +0800

    2

---
+++ app/src/main/java/com/mango/accounts/SecondAcitivity.java
@@ -0,0 +1,13 @@
+package com.mango.accounts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondAcitivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_layout);
+    }
+}
